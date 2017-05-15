beebb5b85798e6c56e1e07c7023f9754d08683dc
Ronash Dhakal <ronashmail@gmail.com>
2017-05-16 00:16:48 +0545

    Param

--- config/params.php
+++ config/params.php
@@ -2,4 +2,10 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'siteName' =>'Nash Social Network',
+    
+    //Roles Variable 
+    'AdminRole' =>'admin',
+    'UserRole' => 'user',
+    'ModeratorRole' => 'moderator',
 ];
