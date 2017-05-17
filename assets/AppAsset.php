3220c3326793129a2620901689d547ddc501f4a6
Ronash Dhakal <ronashmail@gmail.com>
2017-05-17 11:49:12 +0545

    appAsset

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+//        'js/jquery-3.2.1.min.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
