8706df6222f510451faae44e9bd433e42361b8a9
Ronash Dhakal <ronashmail@gmail.com>
2017-05-18 12:43:58 +0545

    module

--- config/modules.php
+++ config/modules.php
@@ -8,18 +8,46 @@
 
 return [
     'gridview' => [ 'class' => '\kartik\grid\Module' ] ,
-    'admin' => [
-            'class' => 'mdm\admin\Module',
-        ],
+   
      'user' => [
         'class' => 'dektrium\user\Module',
          'enableUnconfirmedLogin'=>true,
          'enableConfirmation'=>true,
-          'admins' => ['ronash']
-    ],
+          'admins' => ['ronash'],
+         'controllerMap' => [
+            'recovery' => [
+                'class'  => 'dektrium\user\controllers\RecoveryController',
+                'layout' => '//main-login',
+                
+            ],
+             'registration' => [
+                'class'  => 'dektrium\user\controllers\RegistrationController',
+                'layout' => '//main-login',
+            ],
+             'admin'=>[
+                 'class'=>'dektrium\user\controllers\AdminController',
+                 'layout'=>'//main',
+                 'as access' => [
+                        'class' => 'yii\filters\AccessControl',
+                        'rules' => [
+                            [
+                                'allow' => true,
+                                'roles' => ['admin'],
+                            ],
+                            [
+                                'actions' => ['switch'],
+                                'allow' => true,
+                                'roles' => ['@'],
+                            ],
+                        ],
+                    ],
+             ]
+        ],
+         ],
     'rbac' => 'dektrium\rbac\RbacWebModule',
      'post' => [
             'class' => 'app\modules\post\Post',
        
         ],
+    
     ];
